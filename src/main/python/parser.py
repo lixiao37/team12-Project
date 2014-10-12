24bf6d4b4b474d2f51d2668c9eb2806ebac7a350
MaxJeong <marcus_fenix_360@hotmail.com>
2014-10-12 02:26:37 -0400

    Added beautifulsoup folder and a working copy for parser

--- src/main/python/parser.py
+++ src/main/python/parser.py
@@ -1,9 +1,25 @@
+from bs4 import BeautifulSoup
+import urllib2
+
 class Parser(object):
 	"""Generic Parser Class"""
 
 	def __init__(self):
 		pass
-
+	
+	def url_list(self, search):
+		url = 'http://www.aljazeera.com/Services/Search/?q=' + search
+		content = urllib2.urlopen(url).read()
+		soup = BeautifulSoup(content)
+		list_of_url = []
+	     
+		for anchor in soup.find_all('a'):
+			#print anchor
+			print(anchor.get('href'))
+			list_of_url.append(anchor.get('href'))
+			
+		return list_of_url	
+        
 def get_author_name(url):
 	'''
 	Return the author's name from the given url,
@@ -17,3 +33,7 @@ def get_article_title(url):
 	if title does not exist, then return empty string.
 	'''
 	pass
+
+if __name__ == '__main__':
+	p = Parser()
+	print p.url_list("dad")
