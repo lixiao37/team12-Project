418f21d1ed72cab339c9247a6406469763c2e55a
MaxJeong <marcus_fenix_360@hotmail.com>
2014-10-14 00:06:28 -0400

    Added a function to parser.py that creates a dictionary for collecting some information from the website's page source such as title and author

--- src/main/python/parser.py
+++ src/main/python/parser.py
@@ -15,18 +15,40 @@ class Parser(object):
 
 		for anchor in soup.find_all('a'):
 			#print anchor
-			print(anchor.get('href'))
+			print anchor.get('href')
 			list_of_url.append(anchor.get('href'))
 
 		return list_of_url
 
-def get_meta_data(url):
-	'''
-	Return all the meta info of the given article url,
-	E.g. {"author": "Ali Mehdi", "url": "", title: ""}
-	'''
-	pass
+	def get_meta_data(self, url):
+		'''
+		Return all the meta info of the given article url,
+		E.g. {"author": "Ali Mehdi", "url": "", title: ""}
+		'''
+		content = urllib2.urlopen(url).read()
+		soup = BeautifulSoup(content)	
+		dictionary = {}
+		
+		for anchor in soup.find_all('meta'):
+			if 'title' in str(anchor):
+				dictionary['Title'] = str(anchor.get('content').strip())
+			
+		for anchor in soup.find_all('meta'):
+			if 'author' in str(anchor):
+				dictionary['Author'] = str(anchor.get('content').strip())
+				
+		for anchor in soup.find_all('meta'):
+			if 'LastModifiedDate' in str(anchor):
+				dictionary['Date Modified'] = str(anchor.get('content'))
+				
+		dictionary['html'] = soup
+				
+		for anchor in soup.find_all('link'):
+			if 'canonical' in str(anchor):
+				dictionary['url'] = str(anchor.get('href'))
+				
+		return dictionary
 
 if __name__ == '__main__':
 	p = Parser()
-	print p.url_list("dad")
+	print p.get_meta_data('http://www.aljazeera.com/news/europe/2014/10/british-mps-back-recognition-palestine-20141013214237623951.html')
