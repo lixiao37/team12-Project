c790c5b69ae67ec672d77c5a67a716066aae24e9
Zhen Jiang <jiangz12@hotmail.com>
2014-10-15 16:35:28 -0400

    Added track and display buttons in index page which will trun to the tracking_list and dispaly pages; Added tracking_list function in root which will enable the user to add/delete sources and targets and show the entered sources and targets, note that it doesn't connect to the database; Added display function in root which will show all the articles stored in database; note that it also doesn't connect to the database

--- src/main/python/root.py
+++ src/main/python/root.py
@@ -32,9 +32,13 @@ class Root:
         #return """<center><h1 style="color:#0033CC">Home Screen</h1>
         #This page only requires a valid login.</center>"""
         return """"<html><body bgcolor="pink"><center>
-                    <h1 style="color:#0033CC">Logout Screen</h1>
+                    <h1 style="color:#0033CC">Welcome to memu!</h1>
+                    <input type="button" value="Tracking sources and targets" onclick="location='/tracking_list'">
+                    <input type="button" value="Display articles" onclick="location='/display'">
+                    <br/>
                     <form method="post" action="/auth/logout">
                     <input type="submit" value="Log out" />
+                    </form>
                 </center></body></html>""" % locals()
     
     @cherrypy.expose
@@ -45,6 +49,41 @@ class Root:
             I want to log in the system!
             <input type="button" value="Log in" onClick="location='/auth/login'"/>
         </center></body></html>""" % locals()
+    
+    @cherrypy.expose
+    def tracking_list(self): # This page is http://127.0.0.1:8080/tracking_list
+        return """<html><body bgcolor="pink"><center>
+                    <h1 style="color:#0033CC">This is for tracking!</h1>
+                    Add the website sources below. <br/>
+                    <form method="post" action="" id="form1">
+                    <input type="text" id="sources"/>
+                    <input type="button" value="Add source" type="submit" onclick="form1.action='#Add add_action here!#';form1.submit();"> 
+                    <input type="button" value="Delete source" type="submit" onclick="form1.action='#Add delete_action here!#';form1.submit();">
+                    </form>
+                    <br/>
+                    """ + "function_that_will_show_the_list_of_sources" + """
+                    Add the website targets below. <br/>
+                    <form method="post" action="" id="form2">
+                    <input type="text" id="targets"/>
+                    <input type="button" value="Add target" type="submit" onclick="form2.action='#Add add_action here!#';form2.submit();"/> 
+                    <input type="button" value="Delete target" type="submit" onclick="form2.action='#Add delete_action here!#';form2.submit();"/>
+                    </form>
+                    <br/>
+                    """ + "function_that_will_show_the_list_of_targets" + """
+                    <input type="button" value="Back" onClick="location='/'"/>
+                </center></body></html>""" % locals()
+    
+    @cherrypy.expose
+    def display(self): # This page is http://127.0.0.1:8080/display
+        return """<html><body bgcolor="pink"><center>
+        <h5> This page is for checking the parser and database.</h5>
+            <h1 style="color:#0033CC">Article List</h1>
+            <form method="post" action="#Add show_article action here!#">
+            <input type="submit" value="Show articles"/>
+            </form>
+            <input type="button" value="Back" onClick="location='/'"/>
+        </center></body></html>""" % locals()
+        
     
     @cherrypy.expose
     @require(name_is("chun")) # requires the logged in user to be chun
