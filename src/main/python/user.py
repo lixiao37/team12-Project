3aac83a5104f5e594100623550d0109d17c44616
Mehdi Ali <mmehdi@blackberry.com>
2014-10-22 15:28:10 -0400

    feature: added requirements and modified user.py

--- src/main/python/user.py
+++ src/main/python/user.py
@@ -1,3 +1,4 @@
+import hashlib
 from mongoengine import *
 
 
@@ -7,3 +8,19 @@ class User(Document):
     group = StringField()
     sources = ListField()
     targets = ListField()
+
+if __name__ == '__main__':
+    host = "ds035260.mongolab.com:35260"
+    dbName = "userinterface"
+    username = "admin"
+    password = "admin"
+    conn = connect(dbName, host=host, username=username,
+                        password=password)
+    name = raw_input("Enter name: ")
+    password = raw_input("Enter pass: ")
+    group = raw_input("Enter Group: ")
+    sources = ["aljazeera", "haaretz", "zhen's sources"]
+    targets = ["bbc", "cnn"]
+    p = hashlib.md5()
+    p.update(password)
+    User(name=name, password=p.hexdigest(), group=group, sources=sources, targets=targets).save()
