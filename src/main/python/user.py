104b19b4aa2796553f377675003b4db8f935a942
chunhongcho <chunhong.cho@mail.utoronto.ca>
2014-10-26 06:49:08 -0400

    Feature: Updated the User schema and tracking page Details:     - Split the sources and targets to represent twitter and news urls     - Updated the tracking page to show two sections now (news and twitter)     - Improved code efficiency     - More documentation

--- src/main/python/user.py
+++ src/main/python/user.py
@@ -6,21 +6,33 @@ class User(Document):
     name = StringField(required=True, unique=True)
     password = StringField(required=True)
     group = StringField()
-    sources = ListField()
-    targets = ListField()
+    news_sources = ListField()
+    news_targets = ListField()
+    twitter_sources = ListField()
+    twitter_targets = ListField()
 
 if __name__ == '__main__':
+    # connects to the database
     host = "ds035260.mongolab.com:35260"
     dbName = "userinterface"
     username = "admin"
     password = "admin"
-    conn = connect(dbName, host=host, username=username,
-                        password=password)
+    conn = connect(dbName, host=host, username=username, password=password)
+
+    # prompt in the commandline 
     name = raw_input("Enter name: ")
     password = raw_input("Enter pass: ")
     group = raw_input("Enter Group: ")
-    sources = ["aljazeera", "haaretz", "zhen's sources"]
-    targets = ["bbc", "cnn"]
+    news_sources = ["aljazeera", "haaretz"]
+    news_targets = ["bbc", "cnn"]
+    twitter_sources = ["twitter 1", "twitter 2"]
+    twitter_targets = ["twitter 3", "twitter 4"]
     p = hashlib.md5()
     p.update(password)
-    User(name=name, password=p.hexdigest(), group=group, sources=sources, targets=targets).save()
+    User(name = name, 
+        password = p.hexdigest(), 
+        group = group, 
+        news_sources = news_sources, 
+        news_targets = news_targets, 
+        twitter_sources = twitter_sources, 
+        twitter_targets = twitter_targets).save()
