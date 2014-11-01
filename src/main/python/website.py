7bddf8dcc12b9a37ccfef182186bba45da4d22a8
chunhongcho <chunhong.cho@mail.utoronto.ca>
2014-11-01 19:44:17 -0400

    Feature: added a main section for the purpose of adding things to the database for each document

--- src/main/python/website.py
+++ src/main/python/website.py
@@ -5,3 +5,18 @@ class Website(Document):
 	name = StringField(required=True, unique=True)
 	homepage_url = StringField()
 	country = StringField()
+
+if __name__ == '__main__':
+    # connects to the database
+    host = "ds035260.mongolab.com:35260"
+    dbName = "userinterface"
+    username = "admin"
+    password = "admin"
+    conn = connect(dbName, host=host, username=username, password=password)
+
+    # prompt in the commandline 
+    name = raw_input("Enter name: ")
+    homepage_url = raw_input("Enter homepage_url: ")
+    
+    Website(name = name,
+    	homepage_url = homepage_url).save()
