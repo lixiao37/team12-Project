219af09ebdfd76fbd32975ef9f546ccf927724e6
MaxJeong <marcus_fenix_360@hotmail.com>
2014-11-02 10:31:53 -0500

    feature: returns a binary representation of a website screenshot inside a zip folder

--- src/main/python/screenshot.py
+++ src/main/python/screenshot.py
@@ -1,6 +1,7 @@
 import dryscrape
 import gzip
 import binascii
+import os
 
 class Screenshot(object):
     """Generic Screenshot class"""
@@ -8,7 +9,7 @@ class Screenshot(object):
     def __init__(self):
         self
         
-    def zipBinary(self, base_url): 
+    def convert_to_binary(self, base_url): 
         self.base_url = base_url
         
         # set up a web scraping session
@@ -25,14 +26,17 @@ class Screenshot(object):
         f_write.writelines(f_read)
         f_write.close()
         f_read.close()
+        os.remove('picture.jpg')
         f_read_zip = open('picture.gz', 'rb')
         with f_read_zip:
             byte = f_read_zip.read(1)
             hexadecimal = binascii.hexlify(byte)
             decimal = int(hexadecimal, 16)
             binary = bin(decimal)[2:].zfill(8)
-        return binary
+        f_read_zip.close()
+        os.remove('picture.gz')
+        return binary      
         
 if __name__ == '__main__':
     screen = Screenshot()
-    print screen.zipBinary('http://www.gosugamers.net/hearthstone/news')
+    print screen.convert_to_binary('http://www.aljazeera.com/')
