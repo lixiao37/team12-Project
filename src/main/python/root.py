3c8ee20378652130d3095dddf3a41af7bb2fc798
Zhen Jiang <jiangz12@hotmail.com>
2014-11-07 10:49:03 -0500

    Added parse in root.py and added parse button in track.html

--- src/main/python/root.py
+++ src/main/python/root.py
@@ -7,6 +7,7 @@ from article import Article
 from website import Website
 from citation import Citation
 from authenticator import AuthController, require, member_of, name_is
+from paser import Parser
 
 connect("parser", host="ds039020.mongolab.com:39020", username="admin", password="admin")
 
@@ -223,7 +224,42 @@ class Root:
                                 i += 1
             relation_dict[source] = target_count
         return relation_dict
-
+    
+    @cherrypy.expose
+    def parse(self):
+        source_dict = {}
+        target_dict = {}
+        user = User.objects(name=cherrypy.session["user"]).only('news_sources', 'news_targets').first()
+        news_sources_name = user.news_sources_name
+        news_sources_url = user.news_sources_url
+        news_targets_name = user.news_targets_name
+        news_targets_url = user.news_targets_url
+        if not news_sources_name:
+            return "Fail: Your news source section is not complete"
+        else if not news_targets_name:
+            return "Fail: Your news target section is not complete"
+        else:
+            # store the relationship between name and url in the two dictionaries
+            i = 0
+            t = 0
+            while i < len(news_sources_name):
+                source_dict[news_sources_name[i]] = news_sources_url[i]
+            while t < len(news_targets_name):
+                target_dict[news_targets_name[i]] = news_targets_url[i]
+            # parse the internet by the given sources and targets
+            # now only pase the news websites
+            p = Parser()
+            for s_name, s in source_dict.viewitems():
+                website = p.add_website({"name": s_name, "homepage_url": s})
+                for t_name, t in target_dict.viewitems():
+                    articles = p.searchArticle(t_name, s)
+                    for a in articles:
+                        article_meta = p.get_meta_data(a)
+                        article = p.add_article(article_meta, website)
+                        p.extract_citation(article_meta.get('html'), t, t_name, article)
+            return "Success!"
+        
+        
     @cherrypy.expose
     @require(name_is("chun")) # requires the logged in user to be chun
     def only_for_chun(self):
